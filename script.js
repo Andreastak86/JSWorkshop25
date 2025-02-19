4beeef7783b990ebeea41a1035c12eddbbdd4fa0
AndreasJL86 <andreas@jobloop.no>
2025-02-19 18:18:56 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Hva du skal gjøre:
+// ✅ Hente HTML-elementene (knapp og boks).
+// ✅ Lage en liste med forskjellige farger.
+// ✅ Endre fargen på boksen hver gang knappen trykkes.
+
+
+// Ekstra utfordringer! 🚀
+// Hvis du vil prøve deg på noe mer, kan du teste disse idéene:
+// ✅ Legg til flere farger i listen.
+// ✅ Gjør at fargen byttes tilfeldig i stedet for i en fast rekkefølge.
+// ✅ Endre tekstfargen i tillegg til bakgrunnsfargen.
+
+
